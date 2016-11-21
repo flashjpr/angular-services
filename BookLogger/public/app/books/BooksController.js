15a1e37385aee62270c3753fde75dfbb1a008166
flashjpr <flashjpr@gmail.com>
2016-11-21 03:49:03 +0000

    Implemented angular decorators to change the $log service and have some fun

--- BookLogger/public/app/books/BooksController.js
+++ BookLogger/public/app/books/BooksController.js
@@ -31,6 +31,7 @@
         function getAllDataSuccess(dataArray) {
             vm.allBooks = dataArray[0];
             vm.allReaders = dataArray[1];
+            $log.awesome('Books and readers received successfully.');
         }
 
         function getAllDataError(error) {
